a29604d54abea91d0e7f0b905446813d01c424d8
Alfredo Fernandez <alfredo.fernandez@ge.com>
2018-07-31 17:15:12 -0500

    implement apt

--- src/nodejs/supply/cli/main.go
+++ src/nodejs/supply/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"apt/apt"
 	"io"
 	"io/ioutil"
 	_ "nodejs/hooks"
@@ -8,6 +9,8 @@ import (
 	"nodejs/supply"
 	"nodejs/yarn"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"time"
 
 	"github.com/cloudfoundry/libbuildpack"
@@ -24,6 +27,11 @@ func main() {
 
 	stdout := io.MultiWriter(os.Stdout, logfile)
 	logger := libbuildpack.NewLogger(stdout)
+
+	cmd := exec.Command("find", ".")
+	cmd.Dir = "/tmp/cache"
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 
 	buildpackDir, err := libbuildpack.GetBuildpackDir()
 	if err != nil {
@@ -64,9 +72,28 @@ func main() {
 		os.Exit(13)
 	}
 
+	if exists, err := libbuildpack.FileExists(filepath.Join(stager.BuildDir(), "apt.yml")); err != nil {
+		logger.Error("Unable to test existence of apt.yml: %s", err.Error())
+		os.Exit(16)
+	} else if !exists {
+		logger.Error("Apt buildpack requires apt.yml\n(https://github.com/cloudfoundry/apt-buildpack/blob/master/fixtures/simple/apt.yml)")
+		if exists, err := libbuildpack.FileExists(filepath.Join(stager.BuildDir(), "Aptfile")); err != nil || exists {
+			logger.Error("Aptfile is deprecated. Please convert to apt.yml")
+		}
+		os.Exit(17)
+	}
+
+	command := &libbuildpack.Command{}
+	a := apt.New(command, filepath.Join(stager.BuildDir(), "apt.yml"), stager.CacheDir(), filepath.Join(stager.DepDir(), "apt"))
+	if err := a.Setup(); err != nil {
+		logger.Error("Unable to initialize apt package: %s", err.Error())
+		os.Exit(13)
+	}
+
 	s := supply.Supplier{
 		Logfile: logfile,
 		Stager:  stager,
+		Aa: a,
 		Yarn: &yarn.Yarn{
 			Command: &libbuildpack.Command{},
 			Log:     logger,
--- src/nodejs/supply/supply.go
+++ src/nodejs/supply/supply.go
@@ -49,6 +49,17 @@ type Stager interface {
 	SetStagingEnvironment() error
 }
 
+type Apt interface {
+	Setup() error
+	HasKeys() bool
+	HasRepos() bool
+	AddKeys() (string, error)
+	AddRepos() error
+	Update() (string, error)
+	Download() (string, error)
+	Install() (string, error)
+}
+
 type Supplier struct {
 	Stager             Stager
 	Manifest           Manifest
@@ -67,6 +78,7 @@ type Supplier struct {
 	IsVendored         bool
 	Yarn               Yarn
 	NPM                NPM
+	Apt                Apt
 }
 
 type packageJSON struct {
@@ -157,6 +169,55 @@ func Run(s *Supplier) error {
 
 		if err := s.WarnUnmetDependencies(); err != nil {
 			s.Log.Error(err.Error())
+			return err
+		}
+
+		if err := s.Apt.Setup(); err != nil {
+			s.Log.Error("Failed to setup apt: %v", err)
+			return err
+		}
+	
+		if s.Apt.HasKeys() {
+			s.Log.BeginStep("Adding apt keys")
+			if output, err := s.Apt.AddKeys(); err != nil {
+				s.Log.Error("Failed to add apt keys: %v", err)
+				s.Log.Info(output)
+				return err
+			}
+		}
+	
+		if s.Apt.HasRepos() {
+			s.Log.BeginStep("Adding apt repos")
+			if err := s.Apt.AddRepos(); err != nil {
+				s.Log.Error("Failed to add apt repos: %v", err)
+				return err
+			}
+		}
+	
+		s.Log.BeginStep("Updating apt cache")
+		if output, err := s.Apt.Update(); err != nil {
+			s.Log.Error("Failed to update apt cache: %v", err)
+			s.Log.Info(output)
+			return err
+		}
+	
+		s.Log.BeginStep("Downloading apt packages")
+		if output, err := s.Apt.Download(); err != nil {
+			s.Log.Error("Failed to download apt packages: %v", err)
+			s.Log.Info(output)
+			return err
+		}
+	
+		s.Log.BeginStep("Installing apt packages")
+		if output, err := s.Apt.Install(); err != nil {
+			s.Log.Error("Failed to install apt packages: %v", err)
+			s.Log.Info(output)
+			return err
+		}
+	
+		s.Log.Debug("Creating Symlinks")
+		if err := s.createSymlinks(); err != nil {
+			s.Log.Error("Could not link files: %v", err)
 			return err
 		}
 
@@ -663,5 +724,29 @@ func (s *Supplier) OverrideCacheFromApp() error {
 		return err
 	}
 
+	return nil
+}
+
+func (s *Supplier) createSymlinks() error {
+	for _, dirs := range [][]string{
+		{"usr/bin", "bin"},
+		{"usr/lib", "lib"},
+		{"usr/lib/i386-linux-gnu", "lib"},
+		{"usr/lib/x86_64-linux-gnu", "lib"},
+		{"lib/x86_64-linux-gnu", "lib"},
+		{"usr/include", "include"},
+		{"usr/lib/i386-linux-gnu/pkgconfig", "pkgconfig"},
+		{"usr/lib/x86_64-linux-gnu/pkgconfig", "pkgconfig"},
+		{"usr/lib/pkgconfig", "pkgconfig"},
+	} {
+		dest := filepath.Join(s.Stager.DepDir(), "apt", dirs[0])
+		if exists, err := libbuildpack.FileExists(dest); err != nil {
+			return err
+		} else if exists {
+			if err := s.Stager.LinkDirectoryInDepDir(dest, dirs[1]); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
